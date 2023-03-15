1c91cc7745cb8926ab8e3d5b98aeaedb9e7d7a3b
Yang Chenxing <chenxing.yang@gmail.com>
2023-03-15 22:17:56 +0800

    Update 10.正则表达式匹配.py

--- 10.正则表达式匹配.py
+++ 10.正则表达式匹配.py
@@ -7,47 +7,47 @@
 # @lc code=start
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        if p[0] != '*':
-            mlen = self.match(s, 0, p, 0)
-            return mlen and self.search(s, mlen, p, mlen)
-        else:
-            return bool(self.search(s, 0, p, 0))
+        s = ' ' + s
+        s_len = len(s)
+        dr = [False] * s_len
+        dw = [False] * s_len
+        dw[0] = True
+        for token in self.tokenizer(p):
+            dw, dr = dr, dw
+            if token == '.':
+                dw[0] = False
+                for i in range(1, s_len):
+                    dw[i] = dr[i - 1]
+            elif len(token) == 1:
+                dw[0] = False
+                for i in range(1, s_len):
+                    dw[i] = dr[i - 1] and s[i] == token
+            elif token == '.*':
+                dw[0] = dr[0]
+                for i in range(1, s_len):
+                    dw[i] = dr[i] or dw[i - 1]
+            else:
+                dw[0] = dr[0]
+                for i in range(1, s_len):
+                    dw[i] = (dr[i - 1] or dw[i - 1]) and s[i] == token[0] or dr[i]
+        return dw[s_len - 1]
         
-    def search(self, s: str, sp: int, p: str, pp: int) -> bool:
-        plen = len(p)
-        for pp in range(pp, plen + 1):
-            if pp == plen or p[pp] != '*':
-                break
-        if pp == len(p):
-            return True
-        for i in range(sp, len(s)):
-            mlen = self.match(s, i, p, pp)
-            if mlen:
-                return self.search(s, sp + mlen, p, pp + mlen)
-        return False
-
-    def match(self, s, sp, p, pp):
-        j = pp
-        slen = len(s)
-        plen = len(p)
-        for i in range(sp, slen):
-            if j >= plen:
-                return False
-            if p[j] == '*':
-                return i - sp
-            if p[j] != '.' and p[j] != s[i]:
-                return False
-            j += 1
-        if j < plen and p[j] != '*':
-            return False
-        return slen - sp
-                
+    def tokenizer(self, p):
+        i = 0
+        s = len(p)
+        while i < s:
+            if i + 1 < s and p[i + 1] == '*':
+                yield p[i:i+2]
+                i += 2
+            else:
+                yield p[i]
+                i += 1               
 
 # print(Solution().isMatch('aa', 'a'))
 # print(Solution().isMatch('aa', 'a*'))
 # print(Solution().isMatch('ab', '.*'))
-# print(Solution().isMatch('abcba', 'a.*ab*'))
-print(Solution().isMatch('aab', 'c*a*b'))
+# print(Solution().isMatch('mississippi', 'mis*is*ip*.'))
+# print(Solution().isMatch('aab', 'c*a*b'))
 
 # @lc code=end
 
