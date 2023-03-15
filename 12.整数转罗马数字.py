4beafe80bbb5c392f0d8c078d1d8dac00fa05123
Yang Chenxing <chenxing.yang@gmail.com>
2023-03-16 00:57:32 +0800

    Update 12.整数转罗马数字.py

--- 12.整数转罗马数字.py
+++ 12.整数转罗马数字.py
@@ -5,49 +5,67 @@
 #
 
 # @lc code=start
+
+M = [
+    '',
+    'M',
+    'MM',
+    'MMM'
+]
+C = [
+    '',
+    'C',
+    'CC',
+    'CCC',
+    'CD',
+    'D',
+    'DC',
+    'DCC',
+    'DCCC',
+    'CM'
+]
+X = [
+    '',
+    'X',
+    'XX',
+    'XXX',
+    'XL',
+    'L',
+    'LX',
+    'LXX',
+    'LXXX',
+    'XC'
+]
+I = [
+    '',
+    'I',
+    'II',
+    'III',
+    'IV',
+    'V',
+    'VI',
+    'VII',
+    'VIII',
+    'IX'
+]
+
+
 class Solution:
     def intToRoman(self, num: int) -> str:
         return ''.join(self.gen(num))
 
     def gen(self, num):
-        while num >= 1000:
-            yield 'M'
-            num -= 1000
-        if num >= 900:
-            yield 'CM'
-            num -= 900
-        if num >= 500:
-            yield 'D'
-            num -= 500
-        if num >= 400:
-            yield 'CD'
-            num -= 400
-        while num >= 100:
-            yield 'C'
-            num -= 100
-        if num >= 90:
-            yield 'XC'
-            num -= 90
-        if num >= 50:
-            yield 'L'
-            num -= 50
-        if num >= 40:
-            yield 'XL'
-            num -= 40
-        while num >= 10:
-            yield 'X'
-            num -= 10
-        if num >= 9:
-            yield 'IX'
-            num -= 9
-        if num >= 5:
-            yield 'V'
-            num -= 5
-        if num >= 4:
-            yield 'IV'
-            num -= 4
-        for i in range(num):
-            yield 'I'
+        if num >= 1000:
+            yield M[num // 1000]
+            num %= 1000
+        if num >= 100:
+            yield C[num // 100]
+            num %= 100
+        if num >= 10:
+            yield X[num // 10]
+            num %= 10
+        if num:
+            yield I[num]
 
 
 # print(Solution().intToRoman(3))
@@ -55,6 +73,5 @@ class Solution:
 # print(Solution().intToRoman(9))
 # print(Solution().intToRoman(58))
 # print(Solution().intToRoman(1994))
-        
-# @lc code=end
 
+# @lc code=end
